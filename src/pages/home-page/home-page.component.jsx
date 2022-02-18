b971b9c90a50a45506fc9a477753cbceaccfe0a9
Dmytro <dmytro.siianytsia@gmail.com>
2022-02-18 15:11:55 +0200

    broken homepage into smaller component, style menu-item

---
+++ src/pages/home-page/home-page.component.jsx
@@ -0,0 +1,10 @@
+import { DirectoryMenu } from '../../components/directory-menu/directory-menu.components'
+import './home-page.style.scss'
+
+export const HomePage = () => {
+	return(
+		<div className="homepage">
+			<DirectoryMenu />
+		</div>
+	)
+}
